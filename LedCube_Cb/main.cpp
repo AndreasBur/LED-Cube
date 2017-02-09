66bfed72481fc14b6b9005253d3a80780f9dd2b9
Andreas Burnickl <burnickl@gmx.de>
2017-02-09 22:26:53 +0100

    ...

--- LedCube_Cb/main.cpp
+++ LedCube_Cb/main.cpp
@@ -66,6 +66,7 @@ byte CubeBuffer1[LEDCUBE_NUMBER_OF_LEDS_PER_SIDE][LEDCUBE_NUMBER_OF_LEDS_PER_SID
 byte CubeBuffer2[LEDCUBE_NUMBER_OF_LEDS_PER_SIDE][LEDCUBE_NUMBER_OF_LEDS_PER_SIDE];
 
 // function declaration
+void setVoxelFastStart(byte X, byte Y, byte Z) { bitSet(CubeBuffer1[Y][Z], X); }
 void setVoxelFast(byte X, byte Y, byte Z) { bitSet(CubeBuffer2[Y][Z], X); }
 void clearVoxelFast(byte X, byte Y, byte Z) { bitClear(CubeBuffer2[Y][Z], X); }
 void writeVoxelFast(byte X, byte Y, byte Z, bool Value) { bitWrite(CubeBuffer2[Y][Z], X, Value); }
@@ -78,8 +79,92 @@ char Voxel;
 
 int main()
 {
-    setVoxelFast(1,5,0);
+    setVoxelFastStart(1,5,0);
+    setVoxelFastStart(6,5,0);
+    setVoxelFastStart(1,1,0);
+    setVoxelFastStart(0,0,0);
+    setVoxelFastStart(7,7,0);
 
+    setVoxelFastStart(3,2,0);
+    setVoxelFastStart(3,3,0);
+
+    // original
+    cout << "original" << endl;
+    for(int X = 0; X < LEDCUBE_NUMBER_OF_LEDS_PER_SIDE; X++) {
+        for(int Y = 0; Y < LEDCUBE_NUMBER_OF_LEDS_PER_SIDE; Y++) {
+            Voxel = getVoxelFast(LEDCUBE_NUMBER_OF_LEDS_PER_SIDE - X - 1, Y, 0);
+             printf("%d,", Voxel);
+        }
+        printf("\n");
+    }
+
+    // ‰uﬂerer Kreis
+    for(int a = 0; a <= 6; a++)
+    {
+        writeVoxelFast(0, a, 0, getVoxelFast(0, a+1, 0));
+        //array_trans[0][a] = array[0][a+1];
+        writeVoxelFast(a, 7, 0, getVoxelFast(a+1, 7, 0));
+        //array_trans[a][7] = array[a+1][7];
+    }
+
+    for(int a = 7; a > 0; a--)
+    {
+        writeVoxelFast(7, a, 0, getVoxelFast(7, a-1, 0));
+        //array_trans[7][a] = array[7][a-1];
+        writeVoxelFast(a, 0, 0, getVoxelFast(a-1, 0, 0));
+        //array_trans[a][0] = array[a-1][0];
+    }
+    // 2 ‰uﬂerer Kreis
+    for(int a = 1; a <= 5; a++)
+    {
+        writeVoxelFast(1, a, 0, getVoxelFast(1, a+1, 0));
+        //array_trans[0][a] = array[0][a+1];
+        writeVoxelFast(a, 6, 0, getVoxelFast(a+1, 6, 0));
+        //array_trans[a][7] = array[a+1][7];
+    }
+
+    for(int a = 6; a > 1; a--)
+    {
+        writeVoxelFast(6, a, 0, getVoxelFast(6, a-1, 0));
+        //array_trans[7][a] = array[7][a-1];
+        writeVoxelFast(a, 1, 0, getVoxelFast(a-1, 1, 0));
+        //array_trans[a][0] = array[a-1][0];
+    }
+    // 3 ‰uﬂerer Kreis
+    for(int a = 2; a <= 4; a++)
+    {
+        writeVoxelFast(2, a, 0, getVoxelFast(2, a+1, 0));
+        //array_trans[0][a] = array[0][a+1];
+        writeVoxelFast(a, 4, 0, getVoxelFast(a+1, 4, 0));
+        //array_trans[a][7] = array[a+1][7];
+    }
+
+    for(int a = 5; a > 2; a--)
+    {
+        writeVoxelFast(5, a, 0, getVoxelFast(5, a-1, 0));
+        //array_trans[7][a] = array[7][a-1];
+        writeVoxelFast(a, 2, 0, getVoxelFast(a-1, 2, 0));
+        //array_trans[a][0] = array[a-1][0];
+    }
+    // 4 ‰uﬂerer Kreis
+    for(int a = 3; a <= 3; a++)
+    {
+        writeVoxelFast(3, a, 0, getVoxelFast(3, a+1, 0));
+        //array_trans[0][a] = array[0][a+1];
+        writeVoxelFast(a, 3, 0, getVoxelFast(a+1, 3, 0));
+        //array_trans[a][7] = array[a+1][7];
+    }
+
+    for(int a = 4; a > 3; a--)
+    {
+        writeVoxelFast(4, a, 0, getVoxelFast(4, a-1, 0));
+        //array_trans[7][a] = array[7][a-1];
+        writeVoxelFast(a, 3, 0, getVoxelFast(a-1, 3, 0));
+        //array_trans[a][0] = array[a-1][0];
+    }
+
+    cout << endl;
+    cout << "transform" << endl;
     for(int X = 0; X < LEDCUBE_NUMBER_OF_LEDS_PER_SIDE; X++) {
         for(int Y = 0; Y < LEDCUBE_NUMBER_OF_LEDS_PER_SIDE; Y++) {
             Voxel = getVoxelFastAusgabe(LEDCUBE_NUMBER_OF_LEDS_PER_SIDE - X - 1, Y, 0);
